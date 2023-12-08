f6655091ecff1dfb25a01a57345efaead81b076e
dimitrijemarkovic <dimitrijedidamarkovic@gmail.com>
2023-12-08 10:48:46 -0800

    Added WordleWindow class and file with Wordle words

--- application/wordlewindow.h
+++ application/wordlewindow.h
@@ -2,6 +2,7 @@
 #define WORDLEWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
 
 namespace Ui {
 class WordleWindow;
@@ -15,8 +16,24 @@ public:
     explicit WordleWindow(QWidget *parent = 0);
     ~WordleWindow();
 
+    void newWordle();
+    void setUpBackground();
+    void readQuizWords();
+    void disableLineEdits();
+    void disableHBoxLayout(int i);
+    void enableHBoxLayout(int i);
+    void gameStarted();
+
+private slots:
+    void handleEnterPressed();
+
 private:
     Ui::WordleWindow *ui;
+    QStringList quizWords;
+    QString wordle = "";
+    bool isGameFinished = false;
+    int tryNumber = 0;
+    bool guessed = false;
 };
 
 #endif // WORDLEWINDOW_H
