7f6af561a72035d885627f82e0ef4eafef79b526
Dimitrije Markovic <dimitrijedidamarkovic@gmail.com>
2023-12-18 21:33:30 +0100

    Added shell script for automated starting of server and two clients(for faster testing)

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Postavke
+SERVER_DIR="/home/user/Desktop/pobedi-me-ako-znas/server/build-server-Desktop-Debug"
+APPLICATION_DIR="/home/user/Desktop/pobedi-me-ako-znas/build-application-Desktop-Debug"
+
+# Kompajliranje i pokretanje servera
+cd "$SERVER_DIR" || exit
+make
+qterminal  -e bash -c "./server" &
+
+# Kompajliranje i pokretanje prve aplikacije
+cd "$APPLICATION_DIR" || exit
+make
+qterminal  -e bash -c "./application" &
+
+# Kompajliranje i pokretanje druge aplikacije
+qterminal  -e bash -c  "./application" &
