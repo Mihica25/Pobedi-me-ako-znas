6cde72426e5dbb8ab425627bf3c93c772507ccf7
Isidora Dukic <isidorakikadukic@gmail.com>
2023-12-30 04:31:09 +0100

    Tests for who knows and start menu

--- tests/tests/koznaTest.cpp
+++ tests/tests/koznaTest.cpp
@@ -1,5 +1,60 @@
 #include "catch.hpp"
+#include <QMainWindow>
 
 #include "../../application/koznaui.h"
 #include "../../application/ui_kozna.h"
+
+
+
+TEST_CASE("KoZnaui Constructor") {
+    QMainWindow *m = new QMainWindow();
+    QTcpSocket *socket = new QTcpSocket(m);
+    KoZnaui *kozna = new KoZnaui(m, socket, "Player1", "Player2", true, 0, 0);
+
+    SECTION("Constructor Initialization") {
+
+        REQUIRE(kozna->getPlayer1() == "Player1");
+        REQUIRE(kozna->getPlayer2() == "Player2");
+        REQUIRE(kozna->getPlayer1Points() == 0);
+        REQUIRE(kozna->getPlayer2Points() == 0);
+    }
+
+    SECTION("Time is set correctly") {
+
+        REQUIRE(kozna->getTime() == 45);
+    }
+}
+
+
+TEST_CASE("Button_KoZna Enabling") {
+    QMainWindow *m = new QMainWindow();
+    QTcpSocket *socket = new QTcpSocket(m);
+    KoZnaui *kozna = new KoZnaui(m, socket, "Player1", "Player2", true, 0, 0);
+
+    kozna->enableUi();
+
+    // Check if the button is enabled
+    REQUIRE(kozna->getUI()->pushButtonAns1->isEnabled() == true);
+    REQUIRE(kozna->getUI()->pushButtonAns2->isEnabled() == true);
+    REQUIRE(kozna->getUI()->pushButtonAns3->isEnabled() == true);
+    REQUIRE(kozna->getUI()->pushButtonAns4->isEnabled() == true);
+}
+
+
+TEST_CASE("Button_KoZna Disabling") {
+    QMainWindow *m = new QMainWindow();
+    QTcpSocket *socket = new QTcpSocket(m);
+    KoZnaui *kozna = new KoZnaui(m, socket, "Player1", "Player2", true, 0, 0);
+
+    kozna->disableUi();
+
+    // Check if the button is disabled
+    REQUIRE(kozna->getUI()->pushButtonAns1->isEnabled() == false);
+    REQUIRE(kozna->getUI()->pushButtonAns2->isEnabled() == false);
+    REQUIRE(kozna->getUI()->pushButtonAns3->isEnabled() == false);
+    REQUIRE(kozna->getUI()->pushButtonAns4->isEnabled() == false);
+}
+
+
+
 
--- tests/tests/pocetniekranTest.cpp
+++ tests/tests/pocetniekranTest.cpp
@@ -4,3 +4,18 @@
 #include "../../application/pocetniekran.h"
 #include "../../application/ui_pocetniekran.h"
 
+TEST_CASE("PocetniEkran Constructor", "[PocetniEkran]") {
+
+    PocetniEkran *pocetniekran = new PocetniEkran();
+
+    SECTION("Constructor Initialization") {
+
+        REQUIRE(pocetniekran->getPlayer1Points() == 0);
+        REQUIRE(pocetniekran->getPlayer2Points() == 0);
+    }
+
+    SECTION("UI is properly initialized") {
+        REQUIRE(pocetniekran->getUi() != nullptr);
+    }
+
+}
