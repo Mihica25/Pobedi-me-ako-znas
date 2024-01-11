13977e7d37bf58d84e154c54c17b5b4c1e6d341f
Mihica <mihaela13filipovic@gmail.com>
2024-01-11 02:26:32 +0100

    cmake

---
+++ application/build/CMakeFiles/application.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Desktop/pobedi-me-ako-znas/application/build/application_autogen/EWIEGA46WW/qrc_resources.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/application_autogen/EWIEGA46WW/qrc_resources.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/build/application_autogen/mocs_compilation.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/application_autogen/mocs_compilation.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/cardwidget.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/cardwidget.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/game.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/game.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/infolog.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/infolog.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/kozna.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/kozna.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/koznaui.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/koznaui.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/logindialog.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/logindialog.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/main.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/main.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/mainwindow.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/mainwindow.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/memorija.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/memorija.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/mojbroj.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/mojbroj.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/mojbrojlogic.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/mojbrojlogic.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/pocetniekran.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/pocetniekran.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/podrunda.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/podrunda.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/podrundaui.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/podrundaui.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/pogodista.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/pogodista.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/pogodistaui.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/pogodistaui.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/recko.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/recko.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/reckoui.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/reckoui.cpp.o"
+  "/home/user/Desktop/pobedi-me-ako-znas/application/resultdialog.cpp" "/home/user/Desktop/pobedi-me-ako-znas/application/build/CMakeFiles/application.dir/resultdialog.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "application_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
